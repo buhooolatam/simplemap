14ea750083e99149edfb6402d0c64802e9770689
buhooolatam <131042586+buhooolatam@users.noreply.github.com>
2024-08-05 19:58:06 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -7,39 +7,39 @@ function initMap() {
     });
 
     // Cargar coordenadas desde el archivo JSON
-    fetch('coor1.json').then(response => {
-        if (!response.ok) {
-            throw new Error('Error al cargar el archivo JSON');
-        }
-        return response.json();
-    }).then(data => {
-        data.coordinates.forEach((coord, index) => {
-            const latLng = new google.maps.LatLng(coord[1], coord[0]);
-            const marker = new google.maps.Marker({
-                position: latLng,
-                map: map,
-                icon: {
-                    path: google.maps.SymbolPath.CIRCLE,
-                    scale: 8,
-                    fillColor: "#B0B0B0",
-                    fillOpacity: 1,
-                    strokeWeight: 0
-                }
-            });
+    fetch('coor1.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al cargar el archivo JSON');
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.coordinates.forEach((coord, index) => {
+                const latLng = new google.maps.LatLng(coord[1], coord[0]);
+                const marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 8,
+                        fillColor: "#B0B0B0",
+                        fillOpacity: 1,
+                        strokeWeight: 0
+                    }
+                });
 
-            const infowindow = new google.maps.InfoWindow({
-                content: `<div style="text-align: center;"><button onclick='registerMarker(${coord[1]}, ${coord[0]})'>Registrar</button></div>`
+                marker.addListener('click', () => {
+                    console.log("Marcador clickeado:", latLng.toString());
+                    handleMarkerClick(latLng);
+                });
             });
-
-            marker.addListener('click', () => {
-                console.log("Marcador clickeado:", latLng.toString());
-                infowindow.open(map, marker);
-            });
-        });
-    }).catch(error => console.error('Error al cargar las coordenadas:', error));
+        })
+        .catch(error => console.error('Error al cargar las coordenadas:', error));
 }
 
-function registerMarker(lat, lng) {
-    const coords = `${lat},${lng}`;
-    console.log('Coordenadas capturadas:', coords);
+function handleMarkerClick(latLng) {
+    const coords = `${latLng.lat()},${latLng.lng()}`;
+    console.log('Coordenadas capturadas al hacer clic en el marcador:', coords);
+    // Aquí puedes agregar más lógica si necesitas manejar las coordenadas de manera específica
 }
