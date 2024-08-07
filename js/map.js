ddd345b5fc2920d8cfe6689e6eea39c810a83b7d
buhooolatam <131042586+buhooolatam@users.noreply.github.com>
2024-08-07 12:09:07 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,45 +1,37 @@
+let map;
+
 function initMap() {
-    const map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
-        center: { lat: -17.7333, lng: -63.1333 } // Centro predeterminado
+        center: { lat: -17.7333, lng: -63.1333 }
     });
 
-    fetch('coor1.json')
-        .then(response => response.json())
-        .then(data => {
-            data.coordinates.forEach((coord, index) => {
-                const marker = new google.maps.Marker({
-                    position: { lat: coord[1], lng: coord[0] },
-                    map: map,
-                    icon: getCustomIcon() // Usamos una función para obtener el ícono
-                });
-
-                const infowindow = new google.maps.InfoWindow({
-                    content: `<div id="info${index}">
-                                <button onclick="redirectToGoogle()">Registrar</button>
-                              </div>`
-                });
-
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker);
-                });
+    fetch('coor1.json').then(response => response.json()).then(data => {
+        data.coordinates.forEach((coord, index) => {
+            const marker = new google.maps.Marker({
+                position: { lat: coord[1], lng: coord[0] },
+                map: map
             });
-        })
-        .catch(error => console.error('Error al cargar las coordenadas:', error));
+
+            const infowindow = new google.maps.InfoWindow({
+                content: `<div style="text-align: center;"><button onclick="redirectToGoogle()">Registrar</button></div>`
+            });
+
+            marker.addListener('click', () => {
+                infowindow.open(map, marker);
+            });
+        });
+    }).catch(error => console.error('Error al cargar las coordenadas:', error));
 }
 
 function redirectToGoogle() {
-    console.log('Redireccionando a Google.com');
-    window.location.href = 'https://google.com'; // Redirecciona a Google.com al hacer clic
+    window.location.href = "https://www.google.com";
 }
 
-function getCustomIcon() {
-    return {
-        path: google.maps.SymbolPath.CIRCLE,
-        fillColor: '#D3D3D3',
-        fillOpacity: 1,
-        scale: 6,
-        strokeColor: '#A0A0A0',
-        strokeWeight: 1
-    };
+function goBack() {
+    console.log("Atrás presionado");
+}
+
+function resetMap() {
+    console.log("Resetear presionado");
 }
